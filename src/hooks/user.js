41f2d0de62c68f6624de0019e58acf0f4d553fb8
pongdang <pongpong0121@kakao.com>
2022-10-26 21:14:59 +0900

    ⭐ feat : 토큰 관련 로그인, 회원가입 API 요청 함수 작성

---
+++ src/hooks/user.js
@@ -0,0 +1,27 @@
+import { join } from '../api/join';
+import { signIn } from '../api/signIn';
+import { useTokenContext } from '../contexts/TokenContext';
+
+export function useLogin() {
+  const { setAccessToken } = useTokenContext();
+
+  return {
+    login: async user => {
+      const { access_token } = await signIn(user);
+
+      setAccessToken(access_token);
+    },
+  };
+}
+
+export function useJoin() {
+  const { setAccessToken } = useTokenContext();
+
+  return {
+    join: async user => {
+      const { access_token } = await join(user);
+
+      setAccessToken(access_token);
+    },
+  };
+}
