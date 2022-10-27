7e3ea99509bdd3ff8a5d1eacaf5eec54d6e8ce89
pongdang <pongpong0121@kakao.com>
2022-10-27 15:38:12 +0900

    🛠 fix : todo 입력 후 에러 메시지 사라지지 않는 문제 해결

--- src/hooks/useFormField.js
+++ src/hooks/useFormField.js
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react';
 export function useFormField({ initialValue, validators }) {
   const [value, setValue] = useState(initialValue ?? '');
   const [errorMessage, setErrorMessage] = useState();
-  const [first, setFirst] = useState(true);
+  const [firstRender, setFirstRender] = useState(true);
 
   const onChange = e => setValue(e.target.value);
 
   useEffect(() => {
-    if (first) {
-      setFirst(false);
+    if (firstRender) {
+      setFirstRender(false);
       setErrorMessage('');
       return;
     }
@@ -23,7 +23,7 @@ export function useFormField({ initialValue, validators }) {
     setErrorMessage(undefined);
     // NOTE: 최초 렌더링시 에러 메시지 비활성
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [JSON.stringify(value), JSON.stringify(validators)]);
+  }, [value, JSON.stringify(validators)]);
 
   const clearValue = () => setValue('');
 
@@ -33,5 +33,6 @@ export function useFormField({ initialValue, validators }) {
     errorMessage,
     setValue,
     clearValue,
+    setFirstRender,
   };
 }
